ff64b090bc48cb4b295e7779295d1e3cafa594c3
ARAFAT <108474267+arafatDU@users.noreply.github.com>
2024-09-04 13:00:26 +0600

    MongoDB connected to the project

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
+const MONGODB_URL = process.env.MONGODB_URI;
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+
+
+app.get('/api/urls', (req, res) => {
+  return res.json({"message": "Hello World"});
+})
+
+
+const start = async () => {
+  try {
+    await connectDB(MONGODB_URL);
+    app.listen(PORT, () => {console.log(`App is listening on port ${PORT}`)})
+  } catch (error) {
+    console.log(error);
+    
+  }
+}
+
+start();
